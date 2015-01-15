177f2942d0423f0ce1feeb772b135670fd1c602e
Muye Jiang <muye@ifanr.com>
2015-01-15 16:59:12 +0800

    added mail detail view

--- sealer/mail/urls.py
+++ sealer/mail/urls.py
@@ -3,5 +3,6 @@ import mail.views
 
 urlpatterns = patterns(
 	'',
-	url(r'^$', mail.views.IndexView.as_view()),
-	)
+	url(r'^$', mail.views.IndexView.as_view(), name='index'),
+	url(r'^(?P<pk>\d+)/$', mail.views.MailDetailView.as_view(), name='mail_detail'),
+)
--- sealer/mail/views.py
+++ sealer/mail/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, DetailView, TemplateView
+import mail.models
 # Create your views here.
 
 class IndexView(TemplateView):
 
 	template_name = 'mail/index.html'
+
+
+class MailDetailView(DetailView):
+
+  template_base_name = 'mail/mail_detail.html'
+  model = mail.models.Mail
+
+  def get_context_data(self, **kwargs):
+    context = super(MailDetailView, self).get_context_data(**kwargs)
+    return context
