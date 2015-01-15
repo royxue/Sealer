172dfe9561e7563fa858851adffa18359906152b
Muye Jiang <muye@ifanr.com>
2015-01-15 18:18:53 +0800

    added background image

--- sealer/sealer/settings.py
+++ sealer/sealer/settings.py
@@ -88,7 +88,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (BASE_DIR + '/static/',)
 
 # Template files
 
-TEMPLATE_DIRS = (BASE_DIR + '/templates/')
+TEMPLATE_DIRS = (BASE_DIR + '/templates/',)
